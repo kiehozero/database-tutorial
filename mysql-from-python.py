71694f721551fbdd0310f154cff7d66c91549661
Stuart Saint <stuartsaint242@gmail.com>
2021-01-02 21:57:43 +0000

    end of tutorial

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username
@@ -15,10 +16,12 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        list_of_names = ['Fred', 'Tony']
+        # Prepare a string with the same number of placeholders as in list_of_names
+        format_strings = ",".join(['%s']*len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE Name IN({});".format(format_strings), list_of_names)
+        connection.commit()
+        # commit() saves the changes to the table
 
 finally:
     # Close connection whether above was successful or not
